53f66b0034aaea97992f3e94907bcbbec82e56aa
Robin Kam <robinkam008@gmail.com>
2014-08-19 08:35:15 +0800

    refined the weixinCallback logic.

--- cloud/app.js
+++ cloud/app.js
@@ -14,17 +14,36 @@ app.get('/hello', function(req, res) {
 
 
 app.get('/wechatCallback', function(req, res) {
+//    AV.Cloud.run("wechatCallback", req, {
+//        success: function(data){
+//            //调用成功，得到成功的应答data
+//            console.log(data);
+//        },
+//        error: function(err){
+//            //处理调用失败
+//            console.log(err);
+//        }
+//    });
+    var echoStr = request.params.echostr;
+    console.log(echoStr);
+
+    //valid signature , option
+    var signature = request.params.signature;
+    var timestamp = request.params.timestamp;
+    var nonce = request.params.nonce;
+
+    var token = "RobinKam";
+    var tmpArr = [token, timestamp, nonce];
+    var tmpStr = tmpArr.sort().join('');
+    tmpStr = require('crypto').createHash('sha1').update(tmpStr).digest('hex');
+
+    if( tmpStr == signature ){
+        console.log("Signature: "+echoStr);
+        response.success(echoStr);
+    }else{
+        console.log("Check Signature Failed");
+    }
 //    res.render('wechatCallback', { echoStr: 'RobinKam```' });
-    AV.Cloud.run("wechatCallback", req, {
-        success: function(data){
-            //调用成功，得到成功的应答data
-            console.log(data);
-        },
-        error: function(err){
-            //处理调用失败
-            console.log(err);
-        }
-    });
 });
 
 app.get('/wechatResponseMessage', function(req, res) {
