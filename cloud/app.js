bf5e906f4f4369ea5d737d8783c51694dd4dde02
Robin Kam <robinkam008@gmail.com>
2014-08-19 09:56:08 +0800

    keep refining the wechatCallback handler.

--- cloud/app.js
+++ cloud/app.js
@@ -9,11 +9,15 @@ app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+    var username = req.query.username;
+    if(username)
+        res.render('hello', { message: 'Hello, '+username });
+    else
+        res.render('hello', { message: 'Hello, Guest'})
 });
 
 
-app.get('/wechatCallback*', function(req, res) {
+app.get('/wechatCallback', function(req, res) {
 //    AV.Cloud.run("wechatCallback", req, {
 //        success: function(data){
 //            //调用成功，得到成功的应答data
@@ -25,25 +29,22 @@ app.get('/wechatCallback*', function(req, res) {
 //        }
 //    });
     var echoStr = req.query.echostr;
-    console.log(echoStr);
 
     //valid signature , option
     var signature = req.query.signature;
     var timestamp = req.query.timestamp;
     var nonce = req.query.nonce;
-
     var token = "RobinKam";
     var tmpArr = [token, timestamp, nonce];
-    var tmpStr = tmpArr.sort().join('');
-    tmpStr = require('crypto').createHash('sha1').update(tmpStr).digest('hex');
+    var mySignature = tmpArr.sort().join('');
+    mySignature = require('crypto').createHash('sha1').update(mySignature).digest('hex');
 
-    if( tmpStr == signature ){
+    if( mySignature == signature ){
         console.log("Signature: "+echoStr);
-        res.render('wechatCallback', {echoStr: echoStr});
     }else{
         console.log("Check Signature Failed");
     }
-//    res.render('wechatCallback', { echoStr: 'RobinKam```' });
+    res.render('wechatCallback', {echoStr: echoStr});
 });
 
 app.get('/wechatResponseMessage*', function(req, res) {
