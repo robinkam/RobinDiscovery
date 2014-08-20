b48b0dca9e871c78569815f22d0cdf058e31c7ac
Robin Kam <robinkam008@gmail.com>
2014-08-21 01:37:16 +0800

    use a third party module for wechat API.

--- cloud/app.js
+++ cloud/app.js
@@ -3,12 +3,52 @@ var express = require('express');
 var app = express();
 
 var util = require('util');
-var wechat = require('cloud/wechat.js');
+var myWeixinAPI = require('cloud/myWeixinAPI.js');
 
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
+
+
+var wechat = require('../node_modules/wechat');
+
+app.use(express.query());
+app.use('/wechat', wechat('RobinKam', function (req, res, next) {
+    // 微信输入信息都在req.weixin上
+    var message = req.weixin;
+    if (message.FromUserName === 'diaosi') {
+        // 回复屌丝(普通回复)
+        res.reply('hehe');
+    } else if (message.content === '你好') {
+        //你也可以这样回复text类型的信息
+        res.reply({
+            content: 'Hello',
+            type: 'text'
+        });
+    } else if (message.content === '音乐') {
+        // 回复一段音乐
+        res.reply({
+            type: "music",
+            content: {
+                title: "来段音乐吧",
+                description: "小苹果",
+                musicUrl: "http://music.baidu.com/data/music/file?link=http://yinyueshiting.baidu.com/data2/music/122112390/12012502946800128.mp3?xcode=784e1a71858661f834bbd116c1a8a82f3f8882ee658cd7de&song_id=120125029",
+                hqMusicUrl: "http://music.baidu.com/data/music/file?link=http://yinyueshiting.baidu.com/data2/music/122112390/12012502946800128.mp3?xcode=784e1a71858661f834bbd116c1a8a82f3f8882ee658cd7de&song_id=120125029"
+            }
+        });
+    } else if (message.content === '图文')  {
+        // 回复高富帅(图文回复)
+        res.reply([
+            {
+                title: '你来我家接我吧',
+                description: '这是女神与高富帅之间的对话',
+                picurl: 'http://c.hiphotos.baidu.com/ting/pic/item/203fb80e7bec54e7959af539ba389b504fc26aa7.jpg',
+                url: 'http://music.baidu.com/topic/special/taiminace?pst=jiaodian'
+            }
+        ]);
+    }
+}));
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
@@ -22,7 +62,7 @@ app.get('/hello', function(req, res) {
 app.get('/wechatCallback', function(req, res) {
     console.log('Handling GET request...');
     var echostr=req.query.echostr;
-    var isSignatureValid=wechat.isSignatureValid(req);
+    var isSignatureValid=myWeixinAPI.isSignatureValid(req);
     console.log('Verify Signature Result: '+isSignatureValid);
     if(isSignatureValid){
         res.write(echostr);
@@ -36,12 +76,9 @@ app.post('/wechatCallback', function(req, res) {
     console.log('Handling POST request...');
     console.log('The request original URL: '+req.originalUrl);
     console.log('The request headers: '+util.inspect(req.headers));
-    console.log('The request params:'+util.inspect(req.params));
     console.log('The request query: '+util.inspect(req.query));
-    console.log('The request body: '+util.inspect(req.body));
-    console.log('The request raw body: '+util.inspect(req.rawBody));
 
-    var isSignatureValid=wechat.isSignatureValid(req);
+    var isSignatureValid=myWeixinAPI.isSignatureValid(req);
     console.log('Verify Signature Result: '+isSignatureValid);
     if(!isSignatureValid){
         res.write('Signature validation failed.');
@@ -54,7 +91,7 @@ app.post('/wechatCallback', function(req, res) {
         formData+=data;
     });
     req.on("end",function(){
-        wechat.processMessage(formData, res);
+        myWeixinAPI.processMessage(formData, res);
     });
 });
 
